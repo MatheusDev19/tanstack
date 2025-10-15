47b3df251268d111bbc978ad74e2ddcb2f7c1463
matheus-rodrigues <matheusolvr.dev@gmail.com>
2025-10-15 15:46:29 -0300

    create simple request

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import { Box, Text } from "@mantine/core";
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
-  
+  async function getData() {
+    const url = "http://localhost:3000/posts";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      return response.json();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <>
       <Box>
-        <Text size="lg" >Welcome to Mantine + Vite + React + TS!</Text>
+        <Text size="lg">Welcome to Mantine + Vite + React + TS!</Text>
       </Box>
     </>
   );
