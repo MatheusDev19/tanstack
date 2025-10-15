2bdee6a728c4f6d3063c6360dfe006764c75acc2
matheus-rodrigues <matheusolvr.dev@gmail.com>
2025-10-15 14:57:51 -0300

    create development invironment

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Box, Text } from "@mantine/core";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Box>
+        <Text size="lg" >Welcome to Mantine + Vite + React + TS!</Text>
+      </Box>
+    </>
+  );
+}
+
+export default App;
